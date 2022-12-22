a8f42154469641f85790e06166ae14cbe20b88ab
Envy Noxious <ahmadadli02@student.uns.ac.id>
2022-12-23 00:00:00 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -184,4 +184,18 @@
   </main>
 <?php include 'footer.php'; ?>
 </body>
+<a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+
+	<!-- ALL JS FILES -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="js/jquery.superslides.min.js"></script>
+	<script src="js/images-loded.min.js"></script>
+	<script src="js/isotope.min.js"></script>
+	<script src="js/baguetteBox.min.js"></script>
+	<script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
 </html>
