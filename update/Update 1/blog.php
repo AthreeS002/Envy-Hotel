115b212fa4e6d2a4d5165a295a8106596686faf3
Envy Noxious <ahmadadli02@student.uns.ac.id>
2022-12-23 13:37:26 +0700

    Create blog.php

---
+++ update/Update 1/blog.php
@@ -0,0 +1,163 @@
+<?php
+include 'koneksi.php';
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en"><!-- Basic -->
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+     <!-- Site Metas -->
+    <title>Envy Hotel - Blog</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
+	<!-- Site CSS -->
+    <link rel="stylesheet" href="css/style.css">    
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+	<!-- Start header -->
+	<header class="top-navbar">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container">
+				<a class="navbar-brand" href="index.php">
+					<img src="images/pindah/coba/logo.png" alt="" />
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbars-rs-food">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="menu.php">Room</a></li>
+						<li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
+							<div class="dropdown-menu" aria-labelledby="dropdown-a">
+								<a class="dropdown-item" href="reservation.php">Reservation</a>
+								<a class="dropdown-item" href="gallery.php">Gallery</a>
+							</div>
+						</li>
+						<li class="nav-item active"><a class="nav-link" href="blog.php">Blog</a></li>
+
+							<?php
+								if(isset($_SESSION['nin'])){
+							?>
+								<li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li> 
+							
+							<?php } else {
+									?>
+									<li class="nav-item"><a class="nav-link" href="login.php">Login / Register</a></li> <?php } ?>
+								
+
+
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- End header -->
+	
+	<!-- Start All Pages -->
+	<div class="all-page-title page-breadcrumb">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-lg-12">
+					<h1>Blog</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End All Pages -->
+	
+	<!-- Start blog details -->
+	<div class="blog-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>The Royalty</h2>
+						<p>What is 'Royalty'?</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-8 col-lg-8 col-12">
+					<div class="blog-inner-details-page">
+						<div class="blog-inner-box">
+							<div class="side-blog-img">
+								<img class="img-fluid" src="images/pindah/home3.jpg" alt="">							
+								<div class="date-blog-up">
+									22 Des
+								</div>
+							</div>
+							<div class="inner-blog-detail details-page">
+								<h3>Explanation of Royalty</h3>
+								<ul>
+									<li><i class="zmdi zmdi-account"></i>Posted By : <span>Ahmad 'Adli</span></li>
+									<li>|</li>
+									<li><i class="zmdi zmdi-time"></i>Time : <span>19.30 am</span></li>
+								</ul>
+								<p>A royalty is a legally binding payment made to an individual or company for the ongoing use of their assets, including copyrighted works, franchises, and natural resources. An example of royalties would be payments received by musicians when their original songs are played on the radio or television, used in movies, performed at concerts, bars, and restaurants, or consumed via streaming services. In most cases, royalties are revenue generators specifically designed to compensate the owners of songs or property when they license out their assets for another party's use.</p>
+								<h3>Understanding Royalties</h3>
+								<p>Royalty payments typically constitute a percentage of the gross or net revenues obtained from the use of property. However, they can be negotiated on a case-by-case basis in accordance with the wishes of both parties involved in the transaction.</p>
+								<blockquote>
+									<p>LIndeed, the existence of class, of social hierarchy, is as old as man himself. It prevails in the jungle where strength determines hierarchy; among men, it has also been savagely the same, whereby rulers vested with power through personal combat, or through lineal heritage as in the case of royalty, ravage their subjects.</p>
+								</blockquote>
+								<p>So that's about royalty. Hope this blog can help for the readers. Have a nice day!</p>
+							</div>
+						</div>
+
+						
+					</div>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+	<!-- End details -->
+	
+	<!-- Start Contact info -->
+	<?php include 'footer.php'; ?>
+	<!-- End Footer -->
+	
+	<a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+
+	<!-- ALL JS FILES -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	
+	<script src="js/jquery.superslides.min.js"></script>
+	<script src="js/images-loded.min.js"></script>
+	<script src="js/isotope.min.js"></script>
+	<script src="js/baguetteBox.min.js"></script>
+	<script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+</html>
