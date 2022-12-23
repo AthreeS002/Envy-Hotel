71431a4387e863191b056379f4b94d5b4d761ea4
Envy Noxious <ahmadadli02@student.uns.ac.id>
2022-12-23 13:36:55 +0700

    Create cancel_req.php

---
+++ update/Update 1/admin/cancel_req.php
@@ -0,0 +1,18 @@
+<?php
+include '../koneksi.php';
+    $id = $_GET['id'];
+    $nama_kamar_query = mysqli_query($koneksi, "SELECT * FROM transaksi where id_transaksi = $id");
+    while($item=mysqli_fetch_array($nama_kamar_query)){
+        $nama_kamar = $item['nama_kamar'];
+    };
+    $nin = $_GET['nin'];
+    // echo $nin.$id.$nama_kamar;
+
+    $result = mysqli_query($koneksi, "UPDATE registrasi SET status='Booked' WHERE nin = $nin");
+    $result2 = mysqli_query($koneksi, "UPDATE transaksi SET status='Booking' WHERE id_transaksi = $id");
+    $result3 = mysqli_query($koneksi, "UPDATE kamar SET status='Booked' WHERE nama_kamar = '$nama_kamar'");
+        if ($result and $result2 and $result3){
+            echo "<script>alert('berhasil')</script>";
+            header("location: transaksi.php");
+        }
+?>
